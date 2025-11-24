c85fe2a9f041baa7db120a85f7f5f06d2506ed33
vpbmac <vitorpbarbosa7@gmail.com>
2025-11-24 07:29:01 -0300

    requirements repo pip

---
+++ course/advanced/graph/socialgraph.py
@@ -0,0 +1,11 @@
+# they know each other if they appear in the same comic book 
+
+
+# split off one hero Id beginning of line
+# count how many space-separated numbers are in the line 
+# group by hero id's to add up connections split into multiple lines (sum(other_heros_count) group by hero_id) 
+
+# sort by total connections
+
+# broadcast?
+# filter name lookup dataset by the most popular hero Id to look up the name
